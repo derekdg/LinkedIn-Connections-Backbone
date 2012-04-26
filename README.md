30f0fc7844072306b69370766fe5c2e352c623c1
unknown <Derek@Derek-PC.(none)>
2012-04-25 21:26:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+[LinkedIn-Connections-Backbone](http://derekdg.com/LinkedIn-Connections-Backbone/)
+=====================
+
+Web Site Overview
+---------------------
+
+This mobile site is a proof-of-concept, playground, etc. for accessing the LinkedIn Javascript API. After user authentication, the page flow is as follows:
+
+1. Home page lists the user's LinkedIn Connections
+2. Next page displays the Connection's Full Profile (coming soon)
+
+
+Technology
+---------------------
+
+- Frameworks: [jQuery 1.7.1](http://jquery.com/) using [Backbone.js](http://documentcloud.github.com/backbone/)
+- LinkedIn Connection Data: Uses [LinkedIn JavaScript API](http://developer.linkedin.com/javascript)
+- Tests: [Jasmine BDD](http://pivotal.github.com/jasmine/) (coming soon)
---
+++ js/app.js
@@ -0,0 +1,95 @@
+﻿
+var myConnections = {
+
+	loadConnections: function() {
+	
+		IN.API.Connections("me")
+			.fields("first-name", "last-name", "headline", "picture-url", "industry", "phone-numbers")
+			.result(function(result) {
+				
+				console.log(result);
+				var lnk = [];
+
+				for (var index in result.values) {
+				  profile = result.values[index]
+				  if (!profile.pictureUrl) { profile.pictureUrl = "img/no-photo.png"; } 
+				  
+				var pos = {
+					profileID: profile.id,
+					firstName: profile.firstName,
+					lastName: profile.lastName,
+					pictureUrl:  profile.pictureUrl,
+					headline: profile.headline,
+					industry: profile.industry
+				};
+
+				lnk[index ] = pos;
+			  }
+
+			/////////////////////////////////
+			//define product model
+			/////////////////////////////////
+			var Contact = Backbone.Model.extend({
+				defaults: { }
+			});
+		
+			/////////////////////////////////
+			//define directory collection
+			/////////////////////////////////
+			var Directory = Backbone.Collection.extend({
+				model: Contact
+			});
+		
+			/////////////////////////////////
+			//define individual contact view
+			/////////////////////////////////
+			var ContactView = Backbone.View.extend({
+				tagName: "article",
+				className: "contact-container",
+				template: $("#contactTemplate").html(),
+		
+				render: function () {
+					var tmpl = _.template(this.template);
+					
+					$(this.el).html(tmpl(this.model.toJSON()));
+					return this;
+				}
+			});
+	
+			////////////////////////
+			//define master view
+			////////////////////////
+			var DirectoryView = Backbone.View.extend({
+				el: $("#contacts"),
+		
+				initialize: function () {
+					this.collection = new Directory(lnk);
+					this.render();
+				},
+		
+				render: function () {
+					var that = this;
+					_.each(this.collection.models, function (item) {
+						that.renderContact(item);
+					}, this);
+				},
+		
+				renderContact: function (item) {
+					var contactView = new ContactView({
+						model: item
+					});
+					this.$el.append(contactView.render().el);
+				}
+			});
+		
+			/////////////////////////////////
+			//create instance of master view
+			/////////////////////////////////
+			var directory = new DirectoryView();
+			
+		});
+
+	
+   } //loadConnections
+	
+} //myConnections	
